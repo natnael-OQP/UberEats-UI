7213441a1d0742eb7b9c84805a54106775d9068b
natnael-OQP <makiya112545@gmail.com>
2022-04-19 02:33:25 +0300

    RestaurantList Component created

---
+++ src/components/RestaurantList.js
@@ -0,0 +1,28 @@
+import { StyleSheet, FlatList, View } from 'react-native'
+import RestaurantItem from './RestaurantItem'
+import restaurant from '../assets/data/restaurants'
+
+const RestaurantList = () => {
+    return (
+        <View>
+            <FlatList
+                data={restaurant}
+                renderItem={({ item }) => (
+                    <RestaurantItem
+                        onPress={() => console.log(item.name)}
+                        image={item.image}
+                        name={item.name}
+                        deliveryFee={item.deliveryFee}
+                        minDeliveryTime={item.minDeliveryTime}
+                        maxDeliveryTime={item.maxDeliveryTime}
+                    />
+                )}
+                keyExtractor={(item) => item.id}
+            />
+        </View>
+    )
+}
+
+export default RestaurantList
+
+const styles = StyleSheet.create({})
