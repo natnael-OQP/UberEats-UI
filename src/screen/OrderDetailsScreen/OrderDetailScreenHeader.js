47459fde3f5c461f64528674ffcf12192ffdf3d6
natnael-OQP <makiya112545@gmail.com>
2022-04-20 14:56:51 +0300

    OrderDetailScreenHeader created

--- src/screen/OrderDetailsScreen/OrderDetailScreenHeader.js
+++ src/screen/OrderDetailsScreen/OrderDetailScreenHeader.js
@@ -1,20 +1,31 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
 import orders from '../../assets/data/orders.js'
+import { useNavigation } from '@react-navigation/native'
 
 const OrderDetailScreenHeader = () => {
+    const navigation = useNavigation()
     return (
         <View>
             <Image
                 style={styles.image}
                 source={{ uri: orders[0].Restaurant.image }}
             />
+            <View style={styles.iconContainer}>
+                <Ionicons
+                    onPress={() => navigation.goBack()}
+                    name="arrow-back-circle"
+                    size={35}
+                    color="white"
+                />
+            </View>
             <View style={styles.infoContainer}>
                 <Text style={styles.title}>{orders[0].Restaurant.name}</Text>
                 <Text style={styles.status}>
                     {orders[0].status} &#8226; 2 days ago
                 </Text>
-            </View>{' '}
+            </View>
         </View>
     )
 }
@@ -37,5 +48,10 @@ const styles = StyleSheet.create({
     status: {
         fontSize: 17,
         color: 'gray',
+    },
+    iconContainer: {
+        position: 'absolute',
+        top: 25,
+        left: 15,
     },
 })
