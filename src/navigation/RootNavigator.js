650001d3268d82f1a5f87d29d36c7c739b23841f
natnael-OQP <makiya112545@gmail.com>
2022-04-20 14:55:49 +0300

    chnage screen name

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,4 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import BasketScreen from '../screen/BasketScreen'
+import DishDetailsScreen from '../screen/DishDetailsScreen'
 import HomeScreen from '../screen/HomeScreen'
 import RestaurantDetailsScreen from '../screen/RestaurantDetailsScreen'
 
@@ -6,11 +8,21 @@ export default function RootNavigator() {
     const Stack = createNativeStackNavigator()
 
     return (
-        <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator>
+            <Stack.Screen name="Restaurants" component={HomeScreen} />
             <Stack.Screen
                 name="Restaurant"
                 component={RestaurantDetailsScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="Dish"
+                component={DishDetailsScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="Basket"
+                component={BasketScreen}
                 options={{ headerShown: false }}
             />
         </Stack.Navigator>
