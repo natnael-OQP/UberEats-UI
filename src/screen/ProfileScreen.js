60870d58607830d9a97230d2841d59f7a122029d
natnael-OQP <makiya112545@gmail.com>
2022-04-20 14:56:36 +0300

    ProfileScreen created

--- src/screen/ProfileScreen.js
+++ src/screen/ProfileScreen.js
@@ -1,13 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Button, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import AppTextInput from '../components/AppTextInput'
 
-const ProfileScreen = () => {
+const Profile = () => {
+    const [name, setName] = useState('')
+    const [address, setAddress] = useState('')
+    const [lat, setLat] = useState('0')
+    const [lng, setLng] = useState('0')
+
+    const navigation = useNavigation()
+
     return (
         <View>
-            <Text>ProfileScreen</Text>
+            <Text style={styles.title}>Profile</Text>
+            <AppTextInput
+                value={name}
+                onChangeText={setName}
+                placeholder="Name"
+            />
+            <AppTextInput
+                value={address}
+                onChangeText={setAddress}
+                placeholder="Address"
+            />
+            <AppTextInput
+                value={lat}
+                onChangeText={setLat}
+                placeholder="Latitude"
+                keyboardType="numeric"
+            />
+            <AppTextInput
+                value={lng}
+                onChangeText={setLng}
+                placeholder="Longitude"
+                keyboardType="numeric"
+            />
+            <Button title="Save" />
+            <Text style={{ textAlign: 'center', color: 'red', margin: 10 }}>
+                Sign out
+            </Text>
         </View>
     )
 }
 
-export default ProfileScreen
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: 10,
+    },
+    input: {
+        margin: 10,
+        backgroundColor: 'white',
+        padding: 15,
+        borderRadius: 5,
+    },
+})
 
-const styles = StyleSheet.create({})
+export default Profile
